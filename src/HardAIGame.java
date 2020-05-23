94cf45be612e1fbbe495cc0b207a46011da043fc
caganbakirci <50342677+caganbakirci@users.noreply.github.com>
2020-05-23 16:44:38 -0700

    Initial commit with alpha-beta

--- src/HardAIGame.java
+++ src/HardAIGame.java
@@ -1,269 +1,276 @@
-import java.util.HashMap;
-import java.util.Random;
-import java.util.Map;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 
-public final class HardAIGame extends EasyAIGame {
-
-    public HardAIGame(int s) {
+public final class HardAIGame extends AIGame 
+{
+    public HardAIGame(int s) 
+    {
         super(s);
     }
 
     @Override
-    public int[] position() {
+    public int[] position()
+    {
+    	int[] pos = new int[2];
         char[][] matrix = super.getMatrix();
-        int[] attack = threat(matrix, 'O');
-        if (attack != null) {
-            return attack;
-        }
-        int[] defense = threat(matrix, 'X');
-        if (defense != null) {
-            System.out.println("DEFENSE!!");
-            return defense;
-        }
-
-        for (int i = 0; i < matrix.length; ++i) {
-            for (int j = 0; j < matrix[0].length; ++j) {
-                if (matrix[i][j] == '-') {
-                    matrix[i][j] = 'O';
-                    if (doubleThreat(matrix, 'O')) {
-                        matrix[i][j] = '-';
-                        System.out.println("DOUBLE ATTACK!!");
-                        return new int[]{i, j};
-                    }
-                    matrix[i][j] = '-';
-                }
-            }
-        }
-
-        int turn = super.getTurn();
-        if (turn == 0) {
-            return randomCorner(matrix.length);
-        }
-        int mid = matrix.length / 2;
-
-        if (turn == 1) {
-            if (matrix[mid][mid] == '-') {
-                return new int[]{mid, mid};
-            }
-            else {
-                return randomCorner(matrix.length);
-            }
-        }
-
-        else if (turn == 2 && matrix[mid][mid] == '-') {
-            // Check the corners
-            int lim = matrix.length - 1;
-            HashSet<int[]> availableCorners = new HashSet<int[]>();
-            HashMap<int[], Character> cornerStatus = new HashMap<int[], Character> ();
-            cornerStatus.put(new int[]{0, 0}, matrix[0][0]);
-            cornerStatus.put(new int[]{0, lim}, matrix[0][lim]);
-            cornerStatus.put(new int[]{lim, 0}, matrix[lim][0]);
-            cornerStatus.put(new int[]{lim, lim}, matrix[lim][lim]);
-            int rowNum = 0;
-            int colNum = 0;
-            for (Map.Entry<int[], Character> entry : cornerStatus.entrySet()) {
-                if (entry.getValue() == '-') {
-                    availableCorners.add(entry.getKey());
-                }
-                if (entry.getValue() == 'O') {
-                    int[] temp = entry.getKey();
-                    rowNum = temp[0];
-                    colNum = temp[1];
-                }
-            }
-            if (availableCorners.size() == 2) {
-                return availableCorners.iterator().next();
-            }
-            else {
-                boolean empty = true;
-                // Check row
-                for (int j = 0; empty && j < matrix.length; ++j ) {
-                    if(matrix[rowNum][j] == 'X') {
-                        empty = false;
-                    }
-                }
-                if (empty) {
-                    for (int[] position : availableCorners ) {
-                        if(position[0] == rowNum) {
-                            return position;
-                        }
-                    }
-                }
-                // Check column
-                empty = true;
-                for (int i = 0; empty && i < matrix.length; ++i ) {
-                    if(matrix[i][colNum] == 'X') {
-                        empty = false;
-                    }
-                }
-                if (empty) {
-                    for (int[] position : availableCorners ) {
-                        if(position[1] == colNum) {
-                            return position;
-                        }
-                    }
-                }
-            }
-        }
-        HashSet<int[]> middles = new HashSet<int[]>();
-        middles.add(new int[]{0, mid});
-        middles.add(new int[]{mid, 0});
-        middles.add(new int[]{mid, matrix.length - 1});
-        middles.add(new int[]{matrix.length - 1, mid});
-
-        for (int i = 0; i < matrix.length; ++i) {
-            for (int j = 0; j < matrix[0].length; ++j) {
-                if (matrix[i][j] == '-') {
-                    matrix[i][j] = 'X';
-                    if (doubleThreat(matrix, 'X')) {
-                        matrix[i][j] = '-';
-                        if (super.getDiagonal1() == matrix.length || super.getDiagonal2() == matrix.length) {
-                            for (int[] pos: middles) {
-                                if (matrix[pos[0]][pos[1]] == '-'){
-                                    System.out.println("DOUBLE DEFENSE SPECIAL!!");
-                                    return pos;
-                                }
-                            }
-                        }
-                        else {
-                            System.out.println("DOUBLE DEFENSE NORMAL!!");
-                            return new int[]{i, j};
-                        }
-                    }
-                    matrix[i][j] = '-';
-                }
-            }
-        }
-
-        for (int i = 0; i < matrix.length; ++i) {
-            for (int j = 0; j < matrix[0].length; ++j) {
-                if (matrix[i][j] == '-') {
-                    matrix[i][j] = 'O';
-                    int[] threat = threat(matrix, 'O');
-                    if (threat != null) {
-                        matrix[i][j] = '-';
-                        System.out.println("SINGLE ATTACK!!");
-                        return threat;
-                    }
-                    matrix[i][j] = '-';
-                }
-            }
-        }
-        System.out.println("RANDOM !!");
-        return super.position();
+        int size = super.size();
+        int depth = 6;
+        int p_inf = Integer.MAX_VALUE;
+    	int n_inf = Integer.MIN_VALUE;
+        int[] arr = max_value(matrix, size, n_inf, p_inf, depth);
+        int score = arr[0];
+        pos[0] = arr[1];
+        pos[1] = arr[2];
+        System.out.println("Score-> " + arr[0]);
+        System.out.println("Position-> " + pos[0] + " " + pos[1]);
+        return pos;
     }
-
-    private static int[] randomCorner(int size) {
-        Random r = new Random();
-        int x =  r.nextBoolean() ? size - 1 : 0;
-        int y = r.nextBoolean() ? size - 1 : 0;
-        return new int[] {x, y};
+    
+    
+    private int[] max_value(char[][] board, int size, int alpha, int beta, int depth)
+    {
+    	System.out.println("Checking depth " + depth);
+    	int p_inf = Integer.MAX_VALUE;
+    	int n_inf = Integer.MIN_VALUE;
+    	int row = -1;
+    	int col = -1;
+		char[][] temp = copy(board, size);
+    	
+    	int[] val = new int[3];
+    	if (depth == 0 || super.isOver())//(evaluation_function(board, size) >= 90 || evaluation_function(board, size) <= -90 || depth == 0)
+    	{
+    		int scr = evaluation_function(board, size);
+    		val[0] = scr;
+    		val[1] = row;
+    		val[2] = col;
+    		return val;
+    		//return evaluation_function(board, size)
+    	}
+    	
+    	int value = n_inf;
+    	
+    	List<int[]> action_list = possible_actions(board, size);
+    	printActions(action_list);
+    	int max_score = 0;
+    	
+    	
+    	int len = action_list.size();
+    	for(int i = 0; i < len; i++)
+    	{
+        	int temp_row = action_list.get(i)[0];
+        	int temp_col = action_list.get(i)[1];
+        	temp[temp_row][temp_col] = 'O';
+        	printMatrix(temp, size);
+    		value = Math.max(value, min_value(temp, size, alpha, beta, depth - 1));
+    		if(value > max_score)
+    		{
+    			max_score = value;
+    			row = action_list.get(i)[0];
+    			col = action_list.get(i)[1];
+    		}
+    		if(value >= beta)
+    		{
+    			val[0] = value;
+    			val[1] = row;
+    			val[2] = col;
+    			return val;
+    		}
+    		alpha = Math.max(alpha, value);
+    	}
+		val[0] = value;
+		val[1] = row;
+		val[2] = col;
+		return val;
     }
-
-    private static boolean doubleThreat(char[][] matrix, char c) {
-        int count = 0;
-        if (threatRows(matrix, c) != null) {
-            ++count;
-        }
-        if (threatColumns(matrix, c) != null) {
-            ++count;
-        }
-        if (threatDiagonals(matrix, c) != null) {
-            ++count;
-        }
-        return count >= 2;
+    
+    private int min_value(char[][] board, int size, int alpha, int beta, int depth)
+    {
+    	System.out.println("Checking depth " + depth);
+    	int p_inf = Integer.MAX_VALUE;
+    	int n_inf = Integer.MIN_VALUE;
+    	
+    	if (depth == 0 || super.isOver())//(evaluation_function(board, size) >= 90 || evaluation_function(board, size) <= -90 ||depth == 0)
+    	{
+    		return evaluation_function(board, size);
+    	}
+    	
+    	int value = p_inf;
+    	
+    	List<int[]> action_list = possible_actions(board, size);
+    	int count = 0;
+    	printActions(action_list);
+    	
+    	int len = action_list.size();
+    	for(int i = 0; i < len; i++)
+    	{
+    		char[][] temp = copy(board, size);
+        	int temp_row = action_list.get(i)[0];
+        	int temp_col = action_list.get(i)[1];
+        	temp[temp_row][temp_col] = 'X';
+        	printMatrix(temp, size);
+    		value = Math.min(value, max_value(temp, size, alpha, beta, depth - 1)[0]);
+    		if(value <= alpha)
+    		{
+    			return value;
+    		}
+    		beta = Math.min(beta, value);
+    		count++;
+    	}
+    	return value;
     }
-
-    private static int[] threat(char[][] matrix, char c) {
-        int[] result = threatRows(matrix, c);
-        if (result != null) {
-            return result;
-        }
-        result = threatColumns(matrix, c);
-        if (result != null) {
-            return result;
-        }
-        return threatDiagonals(matrix, c);
+    
+    private List<int[]> possible_actions(char[][] board, int size)
+    {
+    	List<int[]> action = new ArrayList<int[]>();
+    	
+    	for(int i = 0; i < size; i++)
+    	{
+    		for(int j = 0; j < size; j++)
+    		{
+    			if(board[i][j] == '-')// && !super.isOver())
+    			{
+    				action.add(new int[] {i, j});
+    			}
+    		}
+    	}
+    	return action;
     }
-
-    private static int[] threatRows(char[][] matrix, char c) {
-        for (int i = 0; i < matrix.length; ++i) {
-            int dashCount = 0;
-            int dashPosition = 0;
-            int charCount = 0;
-            for (int j = 0; j < matrix[0].length; ++j) {
-                if (matrix[i][j] == c) {
-                    ++charCount;
-                }
-                else if (matrix[i][j] == '-') {
-                    ++dashCount;
-                    dashPosition = j;
-                }
-            }
-            if (dashCount ==  1 && charCount == matrix.length - 1) {
-                return new int[] {i, dashPosition};
-            }
-
-        }
-        return null;
+    
+    private int evaluation_function(char[][] board, int size)
+    {
+    	char[][] trans = transpose(board, size);
+    	int total = 0;
+    	total += check_horizontal(board, size) + check_horizontal(trans, size);
+    	total += check_first_diagonal(board, size);
+    	total += check_second_diagonal(board, size);
+    	return total;
     }
-
-    private static int[] threatColumns(char[][] matrix, char c) {
-        for (int j = 0; j < matrix[0].length; ++j) {
-            int dashCount = 0;
-            int charCount = 0;
-            int dashPosition = 0;
-            for (int i = 0; i < matrix.length; ++i) {
-                if (matrix[i][j] == c) {
-                    ++charCount;
-                }
-                else if (matrix[i][j] == '-') {
-                    ++dashCount;
-                    dashPosition = i;
-                }
-            }
-            if (dashCount ==  1 && charCount == matrix.length - 1) {
-                return new int[] {dashPosition, j};
-            }
-
-        }
-        return null;
+    
+    private int check_horizontal(char[][] board, int size)
+    {
+    	int total = 0;
+    	for (int i = 0; i < size; i++)
+    	{
+    		int x_count = 0;
+    		int o_count = 0;
+    		for(int j = 0; j < size; j++)
+    		{
+    			if(board[i][j] == 'X')
+    			{
+    				x_count++;
+    			}
+    			else if(board[i][j] == 'O')
+    			{
+    				o_count++;
+    			}
+    		}
+    		total += (int)(Math.pow(10, o_count - 1) - Math.pow(10, x_count - 1));
+    		//System.out.println("x_count-> " + x_count);
+    		//System.out.println("o_count-> " + o_count);
+    		//System.out.println("total-> " + total);
+    	}
+    	//System.out.println("FINAL TOTAL: " + total);
+    	return total;
     }
-
-    private static int[] threatDiagonals(char[][] matrix, char c) {
-        int dashCount = 0;
-        int charCount = 0;
-        int dashPosition = 0;
-        for (int i = 0; i < matrix.length; ++i) {
-            if(matrix[i][i] == c) {
-                ++charCount;
-            }
-            else if (matrix[i][i] == '-') {
-                ++dashCount;
-                dashPosition = i;
-            }
-        }
-
-        if (dashCount ==  1 && charCount == matrix.length - 1) {
-            return new int[] {dashPosition, dashPosition};
-        }
-
-        dashCount = 0;
-        charCount = 0;
-        dashPosition = 0;
-        for (int i = 0; i < matrix.length; ++i) {
-            if(matrix[i][matrix.length - i - 1] == c) {
-                ++charCount;
-            }
-            else if(matrix[i][matrix.length - i - 1] == '-') {
-                ++dashCount;
-                dashPosition = i;
-            }
-        }
-        if (dashCount ==  1 && charCount == matrix.length - 1) {
-            return new int[] {dashPosition, matrix.length - dashPosition - 1};
-        }
-        return null;
+    
+    private int check_first_diagonal(char[][] board, int size)
+    {
+    	int total = 0;
+    	for (int i = 0; i < size; i++)
+    	{
+    		int x_count = 0;
+    		int o_count = 0;
+    		for(int j = 0; j < size; j++)
+    		{
+    			if(i == j)
+    			{
+	    			if(board[i][j] == 'X')
+	    			{
+	    				x_count++;
+	    			}
+	    			else if(board[i][j] == 'O')
+	    			{
+	    				o_count++;
+	    			}
+    			}
+    		}
+    		total += (int)(Math.pow(10, o_count - 1) - Math.pow(10, x_count - 1));
+    	}
+    	return total;
+    }
+    
+    private int check_second_diagonal(char[][] board, int size)
+    {
+    	int total = 0;
+    	for (int i = 0; i < size; i++)
+    	{
+    		int x_count = 0;
+    		int o_count = 0;
+    		for(int j = 0; j < size; j++)
+    		{
+    			if(i + j == (size - 1))
+    			{
+	    			if(board[i][j] == 'X')
+	    			{
+	    				x_count++;
+	    			}
+	    			else if(board[i][j] == 'O')
+	    			{
+	    				o_count++;
+	    			}
+    			}
+    		}
+    		total += (int)(Math.pow(10, o_count - 1) - Math.pow(10, x_count - 1));
+    	}
+    	return total;
+    }
+    
+    private char[][] transpose(char[][] board, int size)
+    {
+    	char transpose[][] = new char[size][size];
+    	for(int i = 0; i < size; i++)
+    	{    
+    		for(int j = 0; j < size; j++)
+    		{    
+    			transpose[i][j] = board[j][i];  
+    		}    
+    	}
+    	return transpose;
+    }
+    
+    private void printActions(List<int[]> list)
+    {
+    	int len = list.size();
+    	for(int i = 0; i < len; i++)
+    	{
+    		System.out.print("(" + list.get(i)[0] + "," + list.get(i)[1] + ") ");
+    	}
+    	System.out.println(" ");
+    }
+    
+    private char[][] copy(char[][] board, int size)
+    {
+    	char[][] copy = new char[size][size];
+    	for(int i = 0; i < size; i++)
+    	{
+    		for(int j = 0; j < size; j++)
+    		{
+    			copy[i][j] = board[i][j];
+    		}
+    	}
+    	return copy;
+    }
+    
+    private void printMatrix(char[][] matrix, int size)
+    {
+    	for(int i = 0; i < size; i++)
+    	{
+    		for(int j = 0; j < size; j++)
+    		{
+    			System.out.print(matrix[i][j] + " ");
+    		}
+    		System.out.println();
+    	}
+    	System.out.println(" ");
     }
 }
